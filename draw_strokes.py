ac7e7f244fcf7611631a442382a595e06f052480
hardmaru <hardmaru@gmail.com>
2017-05-17 16:57:25 -0700

    Create draw_strokes.py

---
+++ draw_strokes.py
@@ -0,0 +1,39 @@
+# this snippet of code is derived taken from:
+# https://github.com/hardmaru/sketch-rnn/blob/master/utils.py
+# please use consistent license
+
+import numpy as np
+import random
+
+# libraries required for visualisation:
+from IPython.display import SVG, display
+import svgwrite
+# conda install -c omnia svgwrite=1.1.6 if you don't have this lib
+
+# little function that displays vector images and saves them to .svg
+def draw_strokes(data, factor=0.2, svg_filename = 'sample.svg'):
+  min_x, max_x, min_y, max_y = get_bounds(data, factor)
+  dims = (50 + max_x - min_x, 50 + max_y - min_y)
+  dwg = svgwrite.Drawing(svg_filename, size=dims)
+  dwg.add(dwg.rect(insert=(0, 0), size=dims,fill='white'))
+  lift_pen = 1
+  abs_x = 25 - min_x 
+  abs_y = 25 - min_y
+  p = "M%s,%s " % (abs_x, abs_y)
+  command = "m"
+  for i in xrange(len(data)):
+    if (lift_pen == 1):
+      command = "m"
+    elif (command != "l"):
+      command = "l"
+    else:
+      command = ""
+    x = float(data[i,0])/factor
+    y = float(data[i,1])/factor
+    lift_pen = data[i, 2]
+    p += command+str(x)+","+str(y)+" "
+  the_color = "black"
+  stroke_width = 1
+  dwg.add(dwg.path(p).stroke(the_color,stroke_width).fill("none"))
+  dwg.save()
+  display(SVG(dwg.tostring()))
