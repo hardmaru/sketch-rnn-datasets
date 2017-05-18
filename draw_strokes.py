21ee5bbbe4dbdf17b300cf7fd1128979e72f6da3
hardmaru <hardmaru@gmail.com>
2017-05-17 19:25:20 -0700

    Update draw_strokes.py

--- draw_strokes.py
+++ draw_strokes.py
@@ -10,6 +10,27 @@ from IPython.display import SVG, display
 import svgwrite
 # conda install -c omnia svgwrite=1.1.6 if you don't have this lib
 
+# helper function for draw_strokes
+def get_bounds(data, factor):
+  min_x = 0
+  max_x = 0
+  min_y = 0
+  max_y = 0
+    
+  abs_x = 0
+  abs_y = 0
+  for i in xrange(len(data)):
+    x = float(data[i,0])/factor
+    y = float(data[i,1])/factor
+    abs_x += x
+    abs_y += y
+    min_x = min(min_x, abs_x)
+    min_y = min(min_y, abs_y)
+    max_x = max(max_x, abs_x)
+    max_y = max(max_y, abs_y)
+    
+  return (min_x, max_x, min_y, max_y)
+
 # little function that displays vector images and saves them to .svg
 def draw_strokes(data, factor=0.2, svg_filename = 'sample.svg'):
   min_x, max_x, min_y, max_y = get_bounds(data, factor)
@@ -37,3 +58,37 @@ def draw_strokes(data, factor=0.2, svg_filename = 'sample.svg'):
   dwg.add(dwg.path(p).stroke(the_color,stroke_width).fill("none"))
   dwg.save()
   display(SVG(dwg.tostring()))
+
+# generate a 2D grid of many vector drawings
+def make_grid_svg(s_list, grid_space=10.0, grid_space_x=15.0):
+  def get_start_and_end(x):
+    x = np.array(x)
+    x = x[:, 0:2]
+    x_start = x[0]
+    x_end = x.sum(axis=0)
+    x = x.cumsum(axis=0)
+    x_max = x.max(axis=0)
+    x_min = x.min(axis=0)
+    center_loc = (x_max+x_min)*0.5
+    return x_start-center_loc, x_end
+  x_pos = 0.0
+  y_pos = 0.0
+  result = [[x_pos, y_pos, 1]]
+  for sample in s_list:
+    s = sample[0]
+    grid_loc = sample[1]
+    grid_y = grid_loc[0]*grid_space+grid_space*0.5
+    grid_x = grid_loc[1]*grid_space_x+grid_space_x*0.5
+    start_loc, delta_pos = get_start_and_end(s)
+
+    loc_x = start_loc[0]
+    loc_y = start_loc[1]
+    new_x_pos = grid_x+loc_x
+    new_y_pos = grid_y+loc_y
+    result.append([new_x_pos-x_pos, new_y_pos-y_pos, 0])
+
+    result += s.tolist()
+    result[-1][2] = 1
+    x_pos = new_x_pos+delta_pos[0]
+    y_pos = new_y_pos+delta_pos[1]
+  return np.array(result)
